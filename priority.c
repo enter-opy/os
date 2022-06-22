a15f509e7057ac676b5b421fc3f106fcf3d6a676
Vivek Vijayan <93899655+enter-opy@users.noreply.github.com>
2022-06-22 11:19:02 +0530

    Update priority.c

--- priority.c
+++ priority.c
@@ -61,8 +61,14 @@ int main(void) {
 	a[0][3] = 0;
 	clock = a[0][0] + a[0][1];
 	for (int i = 1; i < n; i++) {
+		if (a[i][0] > clock) {
+			clock += a[i][1];
+			a[i][2] = a[i][1];
+			a[i][3] = 0;
+			continue;
+		}
 		clock += a[i][1];
-		 a[i][2] = clock - a[i][0];
+		a[i][2] = clock - a[i][0];
 		a[i][3] = a[i][2] - a[i][1]; 
 	}
 
